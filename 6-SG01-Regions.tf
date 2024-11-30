3fdc954b45da349458dded690cd6463191a1d50d
statuc30721 <bubalator@yahoo.com>
2024-11-30 11:44:36 +0100

    updated routes for all regions.

---
+++ 6-SG01-Regions.tf
@@ -0,0 +1,691 @@
+# All Security groups are in this one file. Need to find a simpler
+# means to break up the files for better viewing and troubleshooting.
+
+# Security group for Load Balancer
+# Tokyo Region
+resource "aws_security_group" "ASG01-SG01-TYO-LB01" {
+    name = "ASG01-SG01-TYO-LB01"
+    description = "Allow HTTP inbound traffic to Load Balancer."
+    vpc_id = aws_vpc.VPC-A-Tokyo-Test.id
+    provider = aws.tokyo
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "ASG01-SG01-TYO-LB01"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+
+# Security Group for Automatic Scaling Group
+resource "aws_security_group" "ASG01-SG02-TYO-TG80" {
+    name = "ASG01-SG02-TYO-TG80"
+    description = "allow traffic to ASG"
+    vpc_id = aws_vpc.VPC-A-Tokyo-Test.id
+    provider = aws.tokyo
+
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+
+      tags = {
+        Name = "ASG01-SG02-TYO-TG80"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+
+# Security group for EC2 Virtual Machines
+resource "aws_security_group" "ASG01-SG03-TYO-servers" {
+    name = "ASG01-SG03-TYO-servers"
+    description = "Allow SSH and HTTP traffic to production servers"
+    vpc_id = aws_vpc.VPC-A-Tokyo-Test.id
+    provider = aws.tokyo
+
+    ingress {
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    tags = {
+        Name = "ASG01-SG03-TYO-servers"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+
+#----------------------------------------------------#
+# New York Region
+
+# Security group for Load Balancer
+
+resource "aws_security_group" "ASG01-SG01-NY-LB01" {
+    name = "ASG01-SG01-NY-LB01"
+    description = "Allow HTTP inbound traffic to Load Balancer."
+    vpc_id = aws_vpc.VPC-B-NewYork-Test.id
+    provider = aws.newyork
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "ASG01-SG01-NY-LB01"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+
+# Security Group for Automatic Scaling Group
+resource "aws_security_group" "ASG01-SG02-NY-TG80" {
+    name = "ASG01-SG02-NY-TG80"
+    description = "allow traffic to ASG"
+    vpc_id = aws_vpc.VPC-B-NewYork-Test.id
+    provider = aws.newyork
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+
+      tags = {
+        Name = "ASG01-SG02-NY-TG80"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+
+# Security group for EC2 Virtual Machines
+resource "aws_security_group" "ASG01-SG03-NY-servers" {
+    name = "ASG01-SG03-NY-servers"
+    description = "Allow SSH and HTTP traffic to production servers"
+    vpc_id = aws_vpc.VPC-B-NewYork-Test.id
+    provider = aws.newyork
+
+    ingress {
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    tags = {
+        Name = "ASG01-SG03-NY-servers"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+#----------------------------------------------------#
+# London Region
+
+# Security group for Load Balancer
+
+resource "aws_security_group" "ASG01-SG01-LONDON-LB01" {
+    name = "ASG01-SG01-LONDON-LB01"
+    description = "Allow HTTP inbound traffic to Load Balancer."
+    vpc_id = aws_vpc.VPC-C-London-Test.id
+    provider = aws.london
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "ASG01-SG01-LONDON-LB01"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+
+# Security Group for Automatic Scaling Group
+resource "aws_security_group" "ASG01-SG02-LONDON-TG80" {
+    name = "ASG01-SG02-LONDON-TG80"
+    description = "allow traffic to ASG"
+    vpc_id = aws_vpc.VPC-C-London-Test.id
+    provider = aws.london
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+
+      tags = {
+        Name = "ASG01-SG02-LONDON-TG80"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+
+# Security group for EC2 Virtual Machines
+resource "aws_security_group" "ASG01-SG03-LONDON-servers" {
+    name = "ASG01-SG03-LONDON-servers"
+    description = "Allow SSH and HTTP traffic to production servers"
+    vpc_id = aws_vpc.VPC-C-London-Test.id
+    provider = aws.london
+
+    ingress {
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    tags = {
+        Name = "ASG01-SG03-LONDON-servers"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+#----------------------------------------------------------#
+# Sao Paulo Region
+
+# Security group for Load Balancer
+
+resource "aws_security_group" "ASG01-SG01-SAO-LB01" {
+    name = "ASG01-SG01-SAO-LB01"
+    description = "Allow HTTP inbound traffic to Load Balancer."
+    vpc_id = aws_vpc.VPC-D-SaoPaolo-Test.id
+    provider = aws.saopaulo
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "ASG01-SG01-SAO-LB01"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+
+# Security Group for Automatic Scaling Group
+resource "aws_security_group" "ASG01-SG02-SAO-TG80" {
+    name = "ASG01-SG02-SAO-TG80"
+    description = "allow traffic to ASG"
+    vpc_id = aws_vpc.VPC-D-SaoPaolo-Test.id
+    provider = aws.saopaulo
+
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+
+      tags = {
+        Name = "ASG01-SG02-SAO-TG80"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+
+# Security group for EC2 Virtual Machines
+resource "aws_security_group" "ASG01-SG03-SAO-servers" {
+    name = "ASG01-SG03-SAO-servers"
+    description = "Allow SSH and HTTP traffic to production servers"
+    vpc_id = aws_vpc.VPC-D-SaoPaolo-Test.id
+    provider = aws.saopaulo
+
+    ingress {
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    tags = {
+        Name = "ASG01-SG03-SAO-servers"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+#------------------------------------------------------------#
+# Australia Region
+
+# Security group for Load Balancer
+
+resource "aws_security_group" "ASG01-SG01-AUS-LB01" {
+    name = "ASG01-SG01-AUS-LB01"
+    description = "Allow HTTP inbound traffic to Load Balancer."
+    vpc_id = aws_vpc.VPC-E-Australia-Test.id
+    provider = aws.australia
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "ASG01-SG01-AUS-LB01"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+
+# Security Group for Automatic Scaling Group
+resource "aws_security_group" "ASG01-SG02-AUS-TG80" {
+    name = "ASG01-SG02-AUS-TG80"
+    description = "allow traffic to ASG"
+    vpc_id = aws_vpc.VPC-E-Australia-Test.id
+    provider = aws.australia
+
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+
+      tags = {
+        Name = "ASG01-SG02-AUS-TG80"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+
+# Security group for EC2 Virtual Machines
+resource "aws_security_group" "ASG01-SG03-AUS-servers" {
+    name = "ASG01-SG03-AUS-servers"
+    description = "Allow SSH and HTTP traffic to production servers"
+    vpc_id = aws_vpc.VPC-E-Australia-Test.id
+    provider = aws.australia
+
+    ingress {
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    tags = {
+        Name = "ASG01-SG03-AUS-servers"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+#--------------------------------------------------#
+# Hong Kong
+
+# Security group for Load Balancer
+
+resource "aws_security_group" "ASG01-SG01-HongKong-LB01" {
+    name = "ASG01-SG01-HongKong-LB01"
+    description = "Allow HTTP inbound traffic to Load Balancer."
+    vpc_id = aws_vpc.VPC-F-HongKong-Test.id
+    provider = aws.hongkong
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "ASG01-SG01-HongKong-LB01"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+
+# Security Group for Automatic Scaling Group
+resource "aws_security_group" "ASG01-SG02-HongKong-TG80" {
+    name = "ASG01-SG02-HongKong-TG80"
+    description = "allow traffic to ASG"
+    vpc_id = aws_vpc.VPC-F-HongKong-Test
+    provider = aws.hongkong
+
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+
+      tags = {
+        Name = "ASG01-SG02-HongKong-TG80"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+
+# Security group for EC2 Virtual Machines
+resource "aws_security_group" "ASG01-SG03-HongKong-servers" {
+    name = "ASG01-SG03-HongKong-servers"
+    description = "Allow SSH and HTTP traffic to production servers"
+    vpc_id = aws_vpc.VPC-F-HongKong-Test
+    provider = aws.hongkong
+
+    ingress {
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    tags = {
+        Name = "ASG01-SG03-HongKong-servers"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+#--------------------------------------------------#
+# California Region
+
+# Security group for Load Balancer
+
+resource "aws_security_group" "ASG01-SG01-CALI-LB01" {
+    name = "ASG01-SG01-CALI-LB01"
+    description = "Allow HTTP inbound traffic to Load Balancer."
+    vpc_id = aws_vpc.VPC-G-California-Test.id
+    provider = aws.california
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "ASG01-SG01-CALI-LB01"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+
+# Security Group for Automatic Scaling Group
+resource "aws_security_group" "ASG01-SG02-CALI-TG80" {
+    name = "ASG01-SG02-CALI-TG80"
+    description = "allow traffic to ASG"
+    vpc_id = aws_vpc.VPC-G-California-Test.id
+    provider = aws.california
+
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+
+      tags = {
+        Name = "ASG01-SG02-CALI-TG80"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
+
+
+# Security group for EC2 Virtual Machines
+resource "aws_security_group" "ASG01-SG03-CALI-servers" {
+    name = "ASG01-SG03-CALI-servers"
+    description = "Allow SSH and HTTP traffic to production servers"
+    vpc_id = aws_vpc.VPC-G-California-Test.id
+    provider = aws.california
+
+    ingress {
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    tags = {
+        Name = "ASG01-SG03-CALI-servers"
+        Service = "application1"
+        Owner = "Frodo"
+        Planet = "Arda"
+    }
+}
